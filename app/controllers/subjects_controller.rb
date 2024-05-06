be8a90814aaab82041278f90862907f618c631e0
yuyunaginagi <centrepg8@gmail.com>
2024-05-06 21:56:00 +0900

    中間テーブルの修正とビュー編集

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -5,7 +5,8 @@ class SubjectsController < ApplicationController
 
   def create
     @subject = Subject.new(subject_params)
-    # @subject.user = current_user
+    @subject.user_id = current_user.id
+
     if @subject.save
       redirect_to root_path
     else
@@ -14,12 +15,14 @@ class SubjectsController < ApplicationController
   end
 
   def show
+    @subject = Subject.find(params[:id])
+    @user = User.find(@subject.user_id)
   end
 
   private
 
   def subject_params
-    params.require(:subject).permit(:subject_name, :image).merge(user_id: current_user.id)
+    params.require(:subject).permit(:subject_name, :image).merge(user_ids: current_user.id)
   end
 
 end
