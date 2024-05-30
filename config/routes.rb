316f3aca513f425a2008cad156a14c392d40eac4
yuyunaginagi <centrepg8@gmail.com>
2024-05-30 13:48:41 +0900

    コードの見た目修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
-  resources :users,    only: [:index]   # ユーザー登録は管理者側から行うため、ユーザー側にはユーザー一覧の取得機能しかついてない
-  resources :subjects, only: [:index, :new, :create, :show, :destroy] do
+  resources   :users,              only: [:index] # ユーザー登録は管理者側から行うため、ユーザー側にはユーザー一覧の取得機能しかついてない
+  resources   :subjects,           only: [:index, :new, :create, :show, :destroy] do
     resources :teaching_materials, only: [:new, :create, :destroy, :edit, :update]
-    resources :exams, only: [:new, :create, :destroy, :edit, :update]
-    resources :questions, only: [:create, :destroy]
+    resources :exams,              only: [:new, :create, :destroy, :edit, :update]
+    resources :questions,          only: [:create, :destroy]
   end
 end
